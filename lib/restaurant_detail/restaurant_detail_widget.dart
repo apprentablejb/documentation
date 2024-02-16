7039ae656b220c3bf32563bfbb905c0739a6daaa
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-16 18:13:37 +0000

    Updating to latest FlutterFlow output.

--- lib/restaurant_detail/restaurant_detail_widget.dart
+++ lib/restaurant_detail/restaurant_detail_widget.dart
@@ -111,167 +111,158 @@ class _RestaurantDetailWidgetState extends State<RestaurantDetailWidget> {
             centerTitle: false,
             elevation: 0.0,
           ),
-          body: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12.0),
-                    child: Image.network(
-                      restaurantDetailRestaurantsRecord.image,
-                      width: MediaQuery.sizeOf(context).width * 1.0,
-                      height: 230.0,
-                      fit: BoxFit.cover,
-                    ),
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12.0),
+                  child: Image.network(
+                    restaurantDetailRestaurantsRecord.image,
+                    width: MediaQuery.sizeOf(context).width * 1.0,
+                    height: 230.0,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                InkWell(
-                  splashColor: Colors.transparent,
-                  focusColor: Colors.transparent,
-                  hoverColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  onTap: () async {
-                    logFirebaseEvent(
-                        'RESTAURANT_DETAIL_PAGE_title_text_ON_TAP');
-                    logFirebaseEvent('title_text_navigate_to');
+              ),
+              InkWell(
+                splashColor: Colors.transparent,
+                focusColor: Colors.transparent,
+                hoverColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onTap: () async {
+                  logFirebaseEvent('RESTAURANT_DETAIL_PAGE_title_text_ON_TAP');
+                  logFirebaseEvent('title_text_navigate_to');
 
-                    context.pushNamed(
-                      'restaurants_info',
-                      queryParameters: {
-                        'restaurantRef': serializeParam(
-                          widget.restaurantRef,
-                          ParamType.DocumentReference,
-                        ),
-                      }.withoutNulls,
-                      extra: <String, dynamic>{
-                        kTransitionInfoKey: TransitionInfo(
-                          hasTransition: true,
-                          transitionType: PageTransitionType.bottomToTop,
-                        ),
-                      },
-                    );
-                  },
-                  child: Text(
-                    restaurantDetailRestaurantsRecord.name,
-                    style: FlutterFlowTheme.of(context).headlineMedium,
-                  ),
+                  context.pushNamed(
+                    'restaurants_info',
+                    queryParameters: {
+                      'restaurantRef': serializeParam(
+                        widget.restaurantRef,
+                        ParamType.DocumentReference,
+                      ),
+                    }.withoutNulls,
+                    extra: <String, dynamic>{
+                      kTransitionInfoKey: TransitionInfo(
+                        hasTransition: true,
+                        transitionType: PageTransitionType.bottomToTop,
+                      ),
+                    },
+                  );
+                },
+                child: Text(
+                  restaurantDetailRestaurantsRecord.name,
+                  style: FlutterFlowTheme.of(context).headlineMedium,
                 ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 5.0, 8.0),
-                      child: StreamBuilder<List<ReviewsRecord>>(
-                        stream: queryReviewsRecord(
-                          parent: widget.restaurantRef,
-                        ),
-                        builder: (context, snapshot) {
-                          // Customize what your widget looks like when it's loading.
-                          if (!snapshot.hasData) {
-                            return Center(
-                              child: SizedBox(
-                                width: 50.0,
-                                height: 50.0,
-                                child: CircularProgressIndicator(
-                                  valueColor: AlwaysStoppedAnimation<Color>(
-                                    FlutterFlowTheme.of(context).primary,
-                                  ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 5.0, 8.0),
+                    child: StreamBuilder<List<ReviewsRecord>>(
+                      stream: queryReviewsRecord(
+                        parent: widget.restaurantRef,
+                      ),
+                      builder: (context, snapshot) {
+                        // Customize what your widget looks like when it's loading.
+                        if (!snapshot.hasData) {
+                          return Center(
+                            child: SizedBox(
+                              width: 50.0,
+                              height: 50.0,
+                              child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                  FlutterFlowTheme.of(context).primary,
                                 ),
                               ),
-                            );
-                          }
-                          List<ReviewsRecord> timestampTextReviewsRecordList =
-                              snapshot.data!;
-                          return Text(
-                            '⭐(${functions.getAverageRating(timestampTextReviewsRecordList.map((e) => e.rating).toList()).toString()})${timestampTextReviewsRecordList.length.toString()}ratings',
-                            style: FlutterFlowTheme.of(context)
-                                .titleMedium
-                                .override(
-                                  fontFamily: 'Readex Pro',
-                                  color: FlutterFlowTheme.of(context).primary,
-                                ),
-                          );
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 5.0, 8.0),
-                      child: Text(
-                        restaurantDetailRestaurantsRecord.category,
-                        style:
-                            FlutterFlowTheme.of(context).titleMedium.override(
-                                  fontFamily: 'Readex Pro',
-                                  color: FlutterFlowTheme.of(context).primary,
-                                ),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Padding(
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 0.0, 8.0),
-                      child: Text(
-                        'open until${dateTimeFormat(
-                          'jm',
-                          restaurantDetailRestaurantsRecord.close,
-                          locale: FFLocalizations.of(context).languageCode,
-                        )}',
-                        style:
-                            FlutterFlowTheme.of(context).titleMedium.override(
-                                  fontFamily: 'Readex Pro',
-                                  color: FlutterFlowTheme.of(context).primary,
-                                ),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        FFLocalizations.of(context).getText(
-                          'dtv58uxx' /* restaurants descriptions */,
-                        ),
-                        style: FlutterFlowTheme.of(context).labelLarge,
-                      ),
-                      Divider(
-                        height: 32.0,
-                        thickness: 1.0,
-                        color: FlutterFlowTheme.of(context).alternate,
-                      ),
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Text(
-                            FFLocalizations.of(context).getText(
-                              '6frkhpcf' /* Menu items */,
                             ),
-                            style: FlutterFlowTheme.of(context).labelLarge,
-                          ),
-                        ],
-                      ),
-                    ],
+                          );
+                        }
+                        List<ReviewsRecord> timestampTextReviewsRecordList =
+                            snapshot.data!;
+                        return Text(
+                          '⭐(${functions.getAverageRating(timestampTextReviewsRecordList.map((e) => e.rating).toList()).toString()})${timestampTextReviewsRecordList.length.toString()}ratings',
+                          style:
+                              FlutterFlowTheme.of(context).titleMedium.override(
+                                    fontFamily: 'Readex Pro',
+                                    color: FlutterFlowTheme.of(context).primary,
+                                  ),
+                        );
+                      },
+                    ),
                   ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 5.0, 8.0),
+                    child: Text(
+                      restaurantDetailRestaurantsRecord.category,
+                      style: FlutterFlowTheme.of(context).titleMedium.override(
+                            fontFamily: 'Readex Pro',
+                            color: FlutterFlowTheme.of(context).primary,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0.0, 8.0, 0.0, 8.0),
+                    child: Text(
+                      'open until${dateTimeFormat(
+                        'jm',
+                        restaurantDetailRestaurantsRecord.close,
+                        locale: FFLocalizations.of(context).languageCode,
+                      )}',
+                      style: FlutterFlowTheme.of(context).titleMedium.override(
+                            fontFamily: 'Readex Pro',
+                            color: FlutterFlowTheme.of(context).primary,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      FFLocalizations.of(context).getText(
+                        'dtv58uxx' /* restaurants descriptions */,
+                      ),
+                      style: FlutterFlowTheme.of(context).labelLarge,
+                    ),
+                    Divider(
+                      height: 32.0,
+                      thickness: 1.0,
+                      color: FlutterFlowTheme.of(context).alternate,
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text(
+                          FFLocalizations.of(context).getText(
+                            '6frkhpcf' /* Menu items */,
+                          ),
+                          style: FlutterFlowTheme.of(context).labelLarge,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                ListView(
-                  padding: EdgeInsets.zero,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  children: [],
-                ),
-              ],
-            ),
+              ),
+              ListView(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                children: [],
+              ),
+            ],
           ),
         );
       },
